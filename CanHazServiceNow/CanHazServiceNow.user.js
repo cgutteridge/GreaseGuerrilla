c4d4560233694432040cc1d108e3c2534edc40ee
Christopher Gutteridge <totl@soton.ac.uk>
2015-11-30 16:13:29 +0000

    Update for FIJI Added field-hide options

--- CanHazServiceNow/CanHazServiceNow.user.js
+++ CanHazServiceNow/CanHazServiceNow.user.js
@@ -1,17 +1,15 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        CanHazServiceNow
 // @namespace   http://totl.net/
 // @description Service Now Ticket Page Deshonkification
 // @include     https://sotonproduction.service-now.com/incident.do*
+// @include     https://sotonpreproduction.service-now.com/incident.do*
 // @version     1
 // @grant       none
-// @require       https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
 // ==/UserScript==
-$.noConflict();
 
 jQuery( document ).ready(function( $ ) {
-
-
+//     https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
   var idCounter = 0;
   function rollUp( text,type )
   {
@@ -21,21 +19,31 @@ jQuery( document ).ready(function( $ ) {
     return '<div id="'+id+'" data-type="'+type+'" class="isol_rollup">'+text+'</div>';  
   }
 
-  $('select').css( 'width','95%');
+  // change column sizes in first section
+  $('.col-sm-6 .form-group .control-label').removeClass('col-lg-4').addClass( 'col-lg-2').css( 'font-size','90%');
+  $('.col-sm-6 .form-group .input_controls').removeClass('col-lg-5').addClass( 'col-lg-8');
 
-  // work out width for reference input fields
-  $('.vsplit').each( function( i,e ) { 
-    e=$(e);
-    var w = e.find( "select" ).width();
-    if(w>150)
-    {
-      e.find( ".element_reference_input" ).css('width',""+(w-50)+"px");
-    }
-  });
-
-  $('#header_attachment').after( 
-    $('<div style="font-size:150%;font-weight:bold;text-align:center">'+$('#incident\\.short_description').val()+'</div>')
-  );
+  $('.mandatory_populated').addClass("isol_dull_field");
+  $('#element\\.incident\\.number').addClass("isol_dull_field");
+  $('#element\\.incident\\.u_logged_for').addClass("isol_dull_field");
+  $('#element\\.incident\\.location').addClass("isol_dull_field");
+  $('#element\\.incident\\.u_location_of_issue').addClass("isol_dull_field");
+  $('#element\\.incident\\.u_room_of_issue').addClass("isol_dull_field");
+  $('#element\\.incident\\.u_contact_number_of_issue').addClass("isol_dull_field");
+  $('#element\\.incident\\.u_category').addClass("isol_dull_field");
+  $('#element\\.incident\\.u_subcategory').addClass("isol_dull_field");
+  $('#element\\.incident\\.impact').addClass("isol_dull_field");
+  $('#element\\.incident\\.urgency').addClass("isol_dull_field");
+  $('#element\\.incident\\.opened_by').addClass("isol_dull_field");
+  $('#element\\.incident\\.sla_due').addClass("isol_dull_field");
+  $('#element\\.incident\\.contact_type').addClass("isol_dull_field");
+  $('#element\\.incident\\.substitute').addClass("isol_dull_field");
+  $('#element\\.incident\\.time_worked').addClass("isol_dull_field");
+  $('#element\\.incident\\.u_as_request').addClass("isol_dull_field");
+ 
+  // add heading with ticket short desc to the top
+  var titlediv = $('<div style="text-align:center"><span style="font-size:150%;font-weight:bold;">'+$('#incident\\.short_description').val()+'</span></div>');
+  $('#header_attachment').after( titlediv );
 
   // Move back to the notes tab even if this has opened with a different tab open
   document.cookie = "__CJ_tabs2_section_incident=%220%22; path=/";
@@ -47,6 +55,7 @@ jQuery( document ).ready(function( $ ) {
   $('.activity_table').before( table );
   $('.activity_table .activity_header').addClass( 'isol_activity_row' );
   $('.activity_table .activity_data').addClass( 'isol_activity_row' );
+
 
   // build up a dataset (in grid) describing all the stuff so we can show it both ways 
   // around (chrono and rev chrono)
@@ -80,7 +89,7 @@ jQuery( document ).ready(function( $ ) {
       }
       prevdatetxt=datetxt;
 
-      nrow = $('<tr><td class="isol_act_time">'+d[3]+':'+d[4]+'</td><td> - </td><td class="isol_act_title"></td><td class="isol_act_data"><table></table></td></tr>');
+      nrow = $('<tr><td class="isol_act_time">'+d[3]+':'+d[4]+'</td><td>&nbsp;-&nbsp;</td><td class="isol_act_title"></td><td class="isol_act_data"><table></table></td></tr>');
       row.find('.activity_date').remove();
       var title = nrow.find('.isol_act_title');
       title.append( row.find( 'span' ) );
@@ -98,9 +107,9 @@ jQuery( document ).ready(function( $ ) {
       row.find('td td table').appendTo(td);
       nrow.find('.isol_act_data>table').append( row.find('td tr') );
       var h="";
-      row.find('td .activity_field').each( function(i,e){
+      row.find('.activity_field').each( function(i,e){
         e=$(e);
-        h+=e.html();
+        h+="<div>"+e.html()+"</div>";
       });
       var html = "<div>"+h+"</div>";
 
@@ -109,6 +118,7 @@ jQuery( document ).ready(function( $ ) {
 
       // add CR after <br> to make it easier to work with lines of data
       html = html.replace( /<br>/g, '<br>\n' );
+
 
       // rollups
       // blocks starting with ">"
@@ -131,6 +141,7 @@ jQuery( document ).ready(function( $ ) {
       // hyperlink URLs
       var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
       html = html.replace(exp,"<a target='_blank' style='text-decoration: underline' href='$1'>$1</a>"); 
+      if( html.match( /email\.gifx/ )) { tr.addClass('isol_emailSent'); }
 
       td.append($(html));
     }
@@ -198,42 +209,67 @@ jQuery( document ).ready(function( $ ) {
   }
  
   // test button - uncomment for testing
-  //$('#clone_incident_ps').after( $('<button id="test_button" class="form_action_button header action_context" type="submit">test</button>') );
+  //$('#clone_incident_ps').after( $('<button id="test_button" class="form_action_button header action_context btn btn-default" type="submit">test</button>') );
   //$('#test_button').click( function(){ 
   //} );
 
 
-  $('#sysverb_update').before( $('<button id="hidesent_button" class="form_action_button header action_context" type="submit">Hide Sent</button>') );
+  $('#sysverb_update').before( $('<button id="hidesent_button" class="form_action_button header action_context btn btn-default" type="">Hide Sent</button>') );
   $('#hidesent_button').click( function(){ 
     $('.isol_emailSent').hide();
     $('#hidesent_button').hide();
     $('#showsent_button').show();
+    return false;
   });
   $('#hidesent_button').hide();
-  $('#sysverb_update').before( $('<button id="showsent_button" class="form_action_button header action_context" type="submit">Show Sent</button>') );
+  $('#sysverb_update').before( $('<button id="showsent_button" class="form_action_button header action_context btn btn-default" type="">Show Sent</button>') );
   $('#showsent_button').click( function(){ 
     $('.isol_emailSent').show();
     $('#hidesent_button').show();
     $('#showsent_button').hide();
+    return false;
   });
 
+  titlediv.prepend( $('<button id="hidedull_button" class="form_action_button action_context btn btn-default" type="">Hide Fields</button>') );
+  $('#hidedull_button').click( function(){ 
+    $('.isol_dull_field').hide();
+    $('#hidedull_button').hide();
+    $('#showdull_button').show();
+    return false;
+  });
+  titlediv.prepend( $('<button id="showdull_button" class="form_action_button action_context btn btn-default" type="">Show Fields</button>') );
+  $('#showdull_button').click( function(){ 
+    $('.isol_dull_field').show();
+    $('#hidedull_button').show();
+    $('#showdull_button').hide();
+    return false;
+  });
+
+  // different behaviour on new tickets?
+  $('.isol_dull_field').hide();
+  $('#hidedull_button').hide();
+
+
+
   // notes buttons 
-  $('#sysverb_update').before( $('<button id="rnotes_button" class="form_action_button header action_context" type="submit">Latest</button>') );
+  $('#sysverb_update').before( $('<button id="rnotes_button" class="form_action_button header action_context btn btn-default" type="submit">Latest</button>') );
   $('#rnotes_button').click( function(){ 
     $('.tab_caption_text:contains("Notes")').click();
     tableDefault();
     formScrollTo( table );
+    return false;
   } );
-  $('#sysverb_update').before( $('<button id="notes_button" class="form_action_button header action_context" type="submit">Notes</button>') );
+  $('#sysverb_update').before( $('<button id="notes_button" class="form_action_button header action_context btn btn-default" type="submit">Notes</button>') );
   $('#notes_button').click( function(){ 
     $('.tab_caption_text:contains("Notes")').click();
     tableReverse();
     formScrollTo( table );
+    return false;
   } );
 
   // resolve button
 
-  $('#clone_incident_ps').after( $('<button id="resolve_button" class="form_action_button header action_context" type="submit">Resolve</button>') );
+  $('#clone_incident_ps').after( $('<button id="resolve_button" class="form_action_button header action_context btn btn-default" type="submit">Resolve</button>') );
   $('#resolve_button').click(function(){
     $('#incident\\.state').val( 6 );
     onChange('incident.state');
@@ -254,6 +290,7 @@ jQuery( document ).ready(function( $ ) {
     formScrollTo( $("#tabs2_section") );
  
     $('#incident\\.close_notes').focus();	
+    return false;
   });
 
   // Remove email-sent rows as they are kinda useless. Maybe make this a toggle later.
